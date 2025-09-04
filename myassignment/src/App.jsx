31c9d311715c73bdf40d94fd711ffad2ab53924a
chetan <chetansuthar945@gmail.com>
2025-09-04 17:25:40 +0530

    TASK

--- myassignment/src/App.jsx
+++ myassignment/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     setShowMore(false);
   }
 
-  // Toggle description
+  // Toggle description (button click only)
   function handleToggleDescription() {
     setShowMore(!showMore);
   }
@@ -32,15 +32,17 @@ function App() {
         src={sculpture.url}
         alt={sculpture.alt}
         onClick={handleImageClick}
-        onDoubleClick={handleToggleDescription}
         className="sculpture-img"
-        title="Click for next | Double click for details"
+        title="Click to see next image"
       />
 
       {/* Instruction line - always visible */}
-      <p className="hint">
-        👉 Click on image to go next | Double click to show/hide details
-      </p>
+      <p className="hint">👉 Click on image to go next</p>
+
+      {/* Show/Hide Button */}
+      <button className="toggle-btn" onClick={handleToggleDescription}>
+        {showMore ? "Hide Details" : "Show Details"}
+      </button>
 
       {showMore && <p className="description">{sculpture.description}</p>}
     </div>
